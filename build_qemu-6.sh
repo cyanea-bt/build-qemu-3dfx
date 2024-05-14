d93648251417605b2577ee71e1538ac65e0b67f8
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2024-05-14 22:25:42 +0200

    add script for qemu-6

---
+++ build_qemu-6.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# check mingw env
+if [ "$MSYSTEM" != "MINGW64" ]; then 
+  echo "Error: MSYSTEM == $MSYSTEM";
+  echo "Use MINGW64 shell instead!";
+  exit 1; 
+fi
+
+# temporarily downgrade curl
+pacman -U --noconfirm packages/qemu-6/*.zst
+
+# clean up qemu-3dfx
+cd qemu-3dfx && git clean -dfx
+if [ -d "./build" ]; then
+  rm -rf ./build
+fi
+
+if [ -d "./qemu-6-patched" ]; then
+  cd qemu-6-patched
+  git clean -dfx
+  git reset --hard
+  git fetch && git pull
+else
+  git clone https://github.com/cyanea-bt/qemu-6-patched
+  cd qemu-6-patched
+fi
+bash ../scripts/sign_commit ..
+mkdir ../build && cd ../build
+if [ -d "/opt/qemu-6" ]; then
+  rm -rf /opt/qemu-6
+fi
+
+# CFLAGS to compile on gcc 14, not optimal
+EXTRA_C="-Wno-error=maybe-uninitialized -Wno-error=dangling-pointer= -Wno-error=enum-int-mismatch"
+
+../qemu-6-patched/configure --extra-cflags="${EXTRA_C}" --prefix=/opt/qemu-6 --target-list=x86_64-softmmu,i386-softmmu \
+                          --enable-whpx --enable-sdl --enable-sdl-image --disable-gtk --disable-gettext \
+                          --enable-libusb --enable-usb-redir --enable-libnfs --enable-vdi \
+                          --enable-vvfat --enable-virglrenderer --enable-qed \
+                          --enable-gnutls --enable-slirp --enable-tools --enable-libssh --enable-dsound \
+                          --enable-qcow1 --enable-zstd --enable-lzo --enable-vnc --enable-vnc-jpeg \
+                          --enable-vnc-sasl --enable-docs --enable-capstone && make -j8 && make install
+
+# restore curl
+pacman -S --noconfirm mingw-w64-x86_64-curl mingw-w64-i686-curl
