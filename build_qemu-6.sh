15814217c551a6adfc0380a0aeda944890e31c98
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2024-05-15 03:07:31 +0200

    update qemu-6 script

--- build_qemu-6.sh
+++ build_qemu-6.sh
@@ -27,11 +27,7 @@ mkdir ../build && cd ../build
 if [ -d "/opt/qemu-6" ]; then
   rm -rf /opt/qemu-6
 fi
-
-# CFLAGS to compile on gcc 14, not optimal
-EXTRA_C="-Wno-error=maybe-uninitialized -Wno-error=dangling-pointer= -Wno-error=enum-int-mismatch"
-
-../qemu-6-patched/configure --extra-cflags="${EXTRA_C}" --prefix=/opt/qemu-6 --target-list=x86_64-softmmu,i386-softmmu \
+../qemu-6-patched/configure --prefix=/opt/qemu-6 --target-list=x86_64-softmmu,i386-softmmu \
                           --enable-whpx --enable-sdl --enable-sdl-image --disable-gtk --disable-gettext \
                           --enable-libusb --enable-usb-redir --enable-libnfs --enable-vdi \
                           --enable-vvfat --enable-virglrenderer --enable-qed \
